dce10abb0f21ddf3efa54dac7a0d3ac29b90724e
jeyefendi <jeyhunfarajov@gmail.com>
2022-10-20 02:21:13 +0300

    half a lesson completed

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-console.log('Request data...')
+console.log("Request data...");
 
-setTimeout(() => {
-    console.log('Preparing data...')
+// setTimeout(() => {
+//     console.log('Preparing data...')
 
+// const backendData = {
+//     server: 'aws',
+//     port: 2000,
+//     status: 'working'
+// }
+
+// setTimeout(() => {
+//     backendData.modified = true
+//     console.log('Data received', backendData)
+// }, 2000)
+// }, 2000)
+
+const p = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("Preparing data...");
     const backendData = {
-        server: 'aws',
-        port: 2000,
-        status: 'working'
-    }
+      server: "aws",
+      port: 2000,
+      status: "working",
+    };
+    resolve(backendData);
+  }, 2000);
+});
 
+p.then(data => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        backendData.modified = true
-        console.log('Data received', backendData)
-    }, 2000)
-}, 2000)
+      data.modified = true;
+      resolve(data);
+    }, 2000);
+  });
+}).then(clientData => {
+    console.log('Data received', clientData)
+})
